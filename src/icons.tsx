a8752a1fb4213d03c662fee36c1218ac2085a410
kaiser <13858889915@163.com>
2021-01-30 22:19:10 +0800

    图标自动高亮

---
+++ src/icons.tsx
@@ -0,0 +1,14 @@
+import { type } from "os";
+
+type Props = {
+    name:string
+};
+
+const Icon = (props: Props) => { 
+    return (
+        <svg className="icon">
+            <use xlinkHref={ '#'+props.name}/>
+        </svg>
+    );
+};
+export default Icon;
--- src/nav.tsx
+++ src/nav.tsx
@@ -5,11 +5,13 @@ import {
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
+  NavLink
 } from "react-router-dom";
 import x from './icon/money.svg';
 import y from './icon/statistic.svg';
 import z from './icon/tag.svg';
+import Icon from './icons';
 console.log(x,y,z)
 const NavWrapper = styled.div`
 box-shadow:0 0 3px rgba(0,0,0,0.25);
@@ -20,11 +22,21 @@ line-height:20px;
   >li{
     width:33.3333%;
     text-align:center;
+    >a{
     padding:5px;
     display:flex;
     flex-direction:column;
     align-items:center;
-  }
+     &.selected{
+        color:red;
+        .icon{
+            fill:red;
+        };
+    };
+    };
+   
+  };
+  
 }
 `;
 const Nav = () => {
@@ -33,30 +45,24 @@ const Nav = () => {
             <ul>
                 <li>
                     
-                    <Link to="/tags">
-                        <svg className="icon">
-                        <use xlinkHref = "#tag" />
-                        </svg>
+                    <NavLink to="/tags" activeClassName='selected'>
+                        <Icon name = 'tag' />
                         <div>标签页</div>
-                        </Link>
+                        </NavLink>
                 </li>
                 <li>
                     
-                    <Link to="/money">
-                        <svg className="icon">
-                        <use xlinkHref = "#money" />
-                    </svg>
+                    <NavLink to="/money" activeClassName="selected">
+                        <Icon name = "money" />
                     <div>记账页</div>    
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
                     
-                    <Link to="/statistics">
-                        <svg className="icon">
-                        <use xlinkHref = "#statistic" />
-                        </svg>
+                    <NavLink to="/statistics" activeClassName="selected">
+                        <Icon name = 'statistic' />
                         <div>统计页</div>
-                        </Link>
+                        </NavLink>
                 </li>
             </ul>
         </NavWrapper>
